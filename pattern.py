2494b9023eeac07c0cb0dde6b0fad7339aa00e36
chandan <chandanb200317@gmail.com>
2023-12-20 11:17:39 +0530

    space

--- pattern.py
+++ pattern.py
@@ -2,6 +2,5 @@ def print_stars_triangle(rows):
     for i in range(1, rows + 1):
         print("*" * i)
 
-# Example: Print a right-angled triangle of stars with 5 rows
 num_rows = 5
 print_stars_triangle(num_rows)
